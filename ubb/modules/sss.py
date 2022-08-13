3e5672ecbcac8b85182bc006ded146a1db977cbf
royalrebe <108943414+royalrebe@users.noreply.github.com>
2022-08-13 04:33:54 -0500

    Create sss.py

---
+++ ubb/modules/sss.py
@@ -0,0 +1,17 @@
+from CARDINGBOT import smx, OWNER_ID
+from telethon import events
+from telethon.tl.functions.users import GetFullUserRequest
+
+PICC = "https://telegra.ph/file/85fd8d6718c5ded6f4aab.jpg"
+
+@smx.on(events.NewMessage(pattern="/start"))
+async def start(event):              
+    if event.is_private:
+       RizBot = await event.client.get_me()
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheRiZoeL = event.chat_id
+       ssmmd = "Hello, Chutya ! I Am Mia khalifa, Kab aa Rhe Hoo Karne"
+            
+    await smx.send_file(TheRiZoeL,
+                  PICC,
+                  caption=ssmmd)
